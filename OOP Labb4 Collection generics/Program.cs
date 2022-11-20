00daaf8a479f78750c935e6d35350c3e39b8604e
JohnAlbrektsson <john.albrektsson@gmail.com>
2022-11-20 13:41:43 +0100

    Added comments

--- OOP Labb4 Collection generics/Program.cs
+++ OOP Labb4 Collection generics/Program.cs
@@ -7,6 +7,7 @@ namespace OOP_Labb4_Collection_generics
     {
         static void Main(string[] args)
         {
+            //Objects of employee
             Employee E1 = new Employee(101, "John", "Male", 23000);
 
             Employee E2 = new Employee(102, "Hanna", "Female", 21000);
@@ -19,13 +20,14 @@ namespace OOP_Labb4_Collection_generics
 
             #region Stack
             Stack<Employee> empstack = new Stack<Employee>();
-
+            //Pushes employee objects into stack
             empstack.Push(E1);
             empstack.Push(E2);
             empstack.Push(E3);
             empstack.Push(E4);
             empstack.Push(E5);
 
+            //Writes out employees in the stack
             foreach(Employee item in empstack)
             {
                 Console.WriteLine("ID = {0}\n Namn = {1}\n Gender = {2}\n Salary = {3}", item.ID, item.Name, item.Gender,item.Salary);
@@ -34,6 +36,7 @@ namespace OOP_Labb4_Collection_generics
             Console.ReadLine();
             Console.Clear();
             Console.WriteLine("Pop");
+            //Removes employees from the stack through the pop method
             while (empstack.Count > 0)
             {
                
@@ -42,6 +45,7 @@ namespace OOP_Labb4_Collection_generics
                 
                 Console.WriteLine($"Left in stack: {empstack.Count}");
             }
+            //Adds the employees into the stack again
             empstack.Push(E1);
             empstack.Push(E2);
             empstack.Push(E3);
@@ -50,6 +54,7 @@ namespace OOP_Labb4_Collection_generics
             Console.ReadLine();
             Console.Clear();
             Console.WriteLine("Peek");
+            //Peeks method to write out an employee
             for (int i = 2; i >0; i--)
             {
                 Employee e = empstack.Peek();
@@ -57,6 +62,7 @@ namespace OOP_Labb4_Collection_generics
                 Console.WriteLine($"Items left in stack: {empstack.Count}");
 
             }
+            //Checks if the stack contains employee nr3
             if(empstack.Contains(E3))
             {
                 Console.WriteLine("item number 3 is in the stack");
@@ -68,22 +74,25 @@ namespace OOP_Labb4_Collection_generics
             Console.WriteLine("List");
             #endregion
             #region List
+            //Creates a list of employees
             List<Employee> emplist = new List<Employee>();
             emplist.Add(E1);
             emplist.Add(E2);
             emplist.Add(E3);
             emplist.Add(E4);
             emplist.Add(E5);
+            //Checks if the list contains employee nr2
             if (emplist.Contains(E2))
             {
                 Console.WriteLine("Employee2 object exists in the list");
             }
             else Console.WriteLine("Employee2 object does not exist in the list");
             Console.WriteLine("---------------------------------");
-
+            //Finds the first male employee in the list
             Console.WriteLine("First male employee = {0}, ID: {1}",emplist.Find(e => e.Gender == "Male").Name, emplist.Find(e => e.Gender == "Male").ID);
 
             Console.WriteLine("----------------------------------");
+            //Writes out all male employees in the list
             foreach(Employee item in emplist.FindAll(item => item.Gender == "Male"))
             {
                 Console.WriteLine($"Name: {item.Name}, ID: {item.ID}, Salary: {item.Salary}");
